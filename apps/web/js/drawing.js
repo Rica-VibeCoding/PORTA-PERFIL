8bcf27002b9ead0c9874bc351fcd502158790080
Rica-VibeCoding <ricardo.nilton@hotmail.com>
2025-04-06 05:19:29 -0300

    Adiciona js/drawing.js (backup do projeto local)

---
+++ apps/web/js/drawing.js
@@ -0,0 +1,6 @@
+// Módulo de desenho - Barril de exportação
+// Este arquivo serve como ponto central para todas as funções de desenho
+// Em uma versão futura, pode ser convertido para usar import/export apropriados
+
+// Por enquanto, apenas declara que este arquivo foi carregado
+console.log('Módulo de desenho carregado');
